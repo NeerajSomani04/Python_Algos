84a91230a03d077168b9751bcc826fca12c20c7b
Neeraj Somani <neerajsomani04@gmail.com>
2020-10-07 12:51:52 -0400

    Update 387-firstUniqueChar.py

--- Leetcode-Problems/387-firstUniqueChar.py
+++ Leetcode-Problems/387-firstUniqueChar.py
@@ -20,7 +20,48 @@
       non are unique, return -1
 '''
 
+# Actual code --
+# Solution 1:
 
+def first_unique_char(s):
+  hash_table = {}
+  for char in s:
+    if char in hash_table:
+      hash_table[char] += 1
+    else:
+      hash_table[char] = 1
+  
+  for i, char in enumerate(s):
+    if hash_table[char] == 1:
+      return i
+  
+  return -1
+
+## Big-O efficiency --
+# Time complexity : O(n) + O(n) ==> O(2n) ==> O(n) ----> this is linear time
+# space complexity: O(26) ==> O(1)
+    # this is tricky, you might be thinking hash table will store n char of array into hash table, but in reality its not true, at max you can have only 26 char in hash table.
+
+    ## question for Aaron:- why he is referring 32 charachter, while calculating space complexity.
+    
+# Solution 2:
+def first_unique_char(s):
+  for i, char in s:
+    if s.index(char) == s.rindex(char):
+      return i
+  
+  return -1
+
+## Big-O efficiency -- 
+# Time complexity : O(n) * (O(n) + O(n)) ==> O(n) * O(2n) ==> O(n ^ 2) ----> this is quadratic time
+    # each index operation inside a for loop is of O(n) time complexity and for loop itself is O(n) time complexity
+# space complexity: O(1) 
+    # as we are not using any data structure to store data.
         
-        
-        
+
+## Summary -- Solution 2 is less efficient than Solution 1.
+
+
+
+
+

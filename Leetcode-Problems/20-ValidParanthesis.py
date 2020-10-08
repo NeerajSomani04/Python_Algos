aeb79315987930dab480419d017e64359169e4a6
Neeraj Somani <neerajsomani04@gmail.com>
2020-10-08 13:19:12 -0400

    Create 20-ValidParanthesis.py

---
+++ Leetcode-Problems/20-ValidParanthesis.py
@@ -0,0 +1,41 @@
+# In this we are going to solve leetcode #387 problem
+# https://leetcode.com/problems/valid-parentheses/
+
+# we are going to follow a specific process (D, D, P, A, B)
+# Definition, Data, Pseudo code, Actual code, Big-O efficiency
+
+## Definition -- Can be easily understood from leetcode problem statement
+
+## Data (IOEA) -- Input, Output, Edge Cases, Assumptions
+  # Input -- string containing just the characters '(', ')', '{', '}', '[' and ']', 
+  # Output -- "true" if the input string is valid, else "false"
+  # Egde Cases -- empty string is a valid string
+  # Assumptions -- string will contain only paranthesis charachters mentioned above
+
+## In this we are going to use Stack. 
+  - A very important thing to understand in each problem, which data structure and method (process) will give you optimum result.
+  - This can only be possible if you understand the behaviour of each data structure.
+
+''' Pseudo code  --
+      - Break a big string input to smaller chunks, known as chunking
+      - closing the most recently opening paranthesis
+      - 
+'''
+
+
+# What is Naive solution:-
+   # Its a solution that seems very obvious but usually not very efficient
+   # Sometime interviewer wants to check, candidates know naive solution or not. Or build from Naive solution to best solution.
+   # A big part of solving problem is comparing different solutions
+  
+    
+    
+# Actual code --
+## Solution 1:
+
+'''
+Question: notes for Aaron
+  - "missed "}" paranthesis while describing input of the problem
+  - 
+
+'''

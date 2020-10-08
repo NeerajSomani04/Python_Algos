e6b054eb6bf80f8e4a4ead5923db3ee819c0b905
Neeraj Somani <neerajsomani04@gmail.com>
2020-10-08 16:44:23 -0400

    Update 20-ValidParanthesis.py

--- Leetcode-Problems/20-ValidParanthesis.py
+++ Leetcode-Problems/20-ValidParanthesis.py
@@ -1,4 +1,4 @@
-# In this we are going to solve leetcode #387 problem
+# In this we are going to solve leetcode problem #20
 # https://leetcode.com/problems/valid-parentheses/
 
 # we are going to follow a specific process (D, D, P, A, B)
@@ -7,35 +7,53 @@
 ## Definition -- Can be easily understood from leetcode problem statement
 
 ## Data (IOEA) -- Input, Output, Edge Cases, Assumptions
-  # Input -- string containing just the characters '(', ')', '{', '}', '[' and ']', 
+  # Input -- string containing just these characters '(', ')', '{', '}', '[' and ']', 
   # Output -- "true" if the input string is valid, else "false"
-  # Egde Cases -- empty string is a valid string
+  # Egde Cases -- empty string is a valid string, string can have only one paranthesis like '[' or '}'
   # Assumptions -- string will contain only paranthesis charachters mentioned above
 
-## In this we are going to use Stack. 
+'''
+In this we are going to use Stack. 
   - A very important thing to understand in each problem, which data structure and method (process) will give you optimum result.
   - This can only be possible if you understand the behaviour of each data structure.
+  - A stack is an array with more specific rules, Last in First Out
+'''
 
-''' Pseudo code  --
+''' 
+Pseudo code  --
       - Break a big string input to smaller chunks, known as chunking
-      - closing the most recently opening paranthesis
-      - 
+      - for char in string
+        - if opening, push into stack
+        - if closing, pop and compare
+          - if test fails, return false
+        - if all passed, return true
 '''
 
 
-# What is Naive solution:-
-   # Its a solution that seems very obvious but usually not very efficient
-   # Sometime interviewer wants to check, candidates know naive solution or not. Or build from Naive solution to best solution.
-   # A big part of solving problem is comparing different solutions
-  
-    
-    
 # Actual code --
 ## Solution 1:
+class Solution:
+  def isValid(self, s: str) -> bool:
+    p_dict = { '(' : ')' , '{' : '}' ,   '[' : ']' }
+    stack = []
+    for _ in s :
+        if _ in p_dict.keys() :
+          stack.append(_)
+        else:
+          if len(stack) == 0 :
+            return False
+          if p_dict[stack.pop()] != _ :
+            return False
+    if len(stack) > 0 :
+        return False
+    else:
+        return True 
+      
 
-'''
-Question: notes for Aaron
-  - "missed "}" paranthesis while describing input of the problem
-  - 
+''' 
+Big-O Efficiency -->
 
-'''
+# time complexity --> O(n) because of for loop
+# space complexity --> O(n) because stack array needs to hold all paranthesis, if needed
+
+

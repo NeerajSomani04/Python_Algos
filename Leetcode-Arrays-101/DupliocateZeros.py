3727c0cb312165c540259fba30f9d66f2b009af5
Neeraj Somani <neerajsomani04@gmail.com>
2020-10-09 13:22:57 -0400

    Update DupliocateZeros.py

--- Leetcode-Arrays-101/DupliocateZeros.py
+++ Leetcode-Arrays-101/DupliocateZeros.py
@@ -11,3 +11,30 @@ Data -
 Pseudo code - 
   - read array
 '''
+
+# Actual Code 
+# Solution 1 -
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        print(arr)
+        
+        len_ = len(arr)
+        i = 0
+        while( i < len_):
+            if arr[i] == 0:
+                arr.insert(i, 0)
+                arr.pop()
+                i += 2
+                continue
+            else:
+                i += 1
+        print(arr)
+
+# Big-O efficiency -
+# time complexity - O(n^2) --> while loop is O(n) and insert operation is O(n)
+# space complexity - O(1) --> no extra space needed.
+
+# Solution 2 -

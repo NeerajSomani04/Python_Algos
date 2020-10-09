61fbccbdb7e15f818d94ce958660cc78fbb62c98
Neeraj Somani <neerajsomani04@gmail.com>
2020-10-09 11:06:42 -0400

    Create DupliocateZeros.py

---
+++ Leetcode-Arrays-101/DupliocateZeros.py
@@ -0,0 +1,13 @@
+# Lets follow our procedure to solve this question:
+'''
+Definition - can be understood from question
+
+Data - 
+  - Input - a fixed length array that contains some intergers
+  - output - function should modify the array in-place, should not return anything
+  - Edge case - no zero at all in the array, at-least 1 element will be in array, zero could be last element in the array, or a zero present on the boundary of the leftover elements.
+  - assumptions - 
+  
+Pseudo code - 
+  - read array
+'''

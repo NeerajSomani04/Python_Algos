0a3048bfb41f03f9f5201a57679db9f4d043814c
Neeraj Somani <neerajsomani04@gmail.com>
2020-10-09 15:45:03 -0400

    Create MergeSortedArray.py

---
+++ Leetcode-Arrays-101/MergeSortedArray.py
@@ -0,0 +1,13 @@
+'''
+Definition - from question itself
+https://leetcode.com/explore/learn/card/fun-with-arrays/525/inserting-items-into-an-array/3253/
+
+Data
+  - input - two sorted integer array
+  - ouput - one fully sorted array
+  - edge cases - 
+  - assumption - these assumptions provided in question itself
+      -- The number of elements initialized in nums1 and nums2 are m and n respectively.
+      -- You may assume that nums1 has enough space (size that is equal to m + n) to hold additional elements from nums2.
+
+Pseudo code -

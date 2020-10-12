3b9c3c55d7c0d0532dc325c188bce052f25e4876
Neeraj Somani <neerajsomani04@gmail.com>
2020-10-12 12:50:54 -0400

    Update RemoveElements.py

--- Leetcode-Arrays-101/RemoveElements.py
+++ Leetcode-Arrays-101/RemoveElements.py
@@ -1,3 +1,24 @@
+# Lets follow our procedure to solve this question:
+
+'''
+Definition - can be understood from question (https://leetcode.com/explore/learn/card/fun-with-arrays/526/deleting-items-from-an-array/3247/)
+
+Data - 
+  - Input - a fixed length array that contains some intergers and a value that needs to be deleted from array
+  - output - return final length of array after deleting value
+  - Edge case - val element not at all present in array, all elements of the array are same as value, val element could be in start and end of the array
+  - assumptions - array should not be empty
+  
+Pseudo code - 
+    - read array and count number of occurance of val element.
+    - reduce that count from array length, that will become our output array length
+    - start reading elements of array from last,
+        - if array element equals val, then increase val_count
+        - else, place that specific array element towards end of array
+  - read the array in reverse order and copy all elements as per output length array.
+'''
+
+# Actual Code 
 # Solution 1:
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:

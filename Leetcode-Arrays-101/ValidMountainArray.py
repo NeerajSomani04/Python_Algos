a80d423b03de508e11dfe81f8661f0cb4b3aeef8
Neeraj Somani <neerajsomani04@gmail.com>
2020-10-12 18:16:24 -0400

    Update ValidMountainArray.py

--- Leetcode-Arrays-101/ValidMountainArray.py
+++ Leetcode-Arrays-101/ValidMountainArray.py
@@ -58,5 +58,30 @@ class Solution:
 
 ''' Big-O efficiency -
 # time complexity - O(n) -- because of count function and for loop
-# space complexity - O(1) 
+# space complexity - O(1) - 
+'''
+
+# Solution 2 -
+class Solution(object):
+    def validMountainArray(self, A):
+        N = len(A)
+        i = 0
+
+        # walk up
+        while i+1 < N and A[i] < A[i+1]:
+            i += 1
+
+        # peak can't be first or last
+        if i == 0 or i == N-1:
+            return False
+
+        # walk down
+        while i+1 < N and A[i] > A[i+1]:
+            i += 1
+
+        return i == N-1
+ 
+''' Big-O efficiency -
+# time complexity - O(n) -- because of count function and for loop
+# space complexity - O(1) -- memory usage is even better
 '''

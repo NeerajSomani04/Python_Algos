cab8456d30221a6c0954e8cdf845c884f40cc9cc
Neeraj Somani <neerajsomani04@gmail.com>
2020-10-13 16:51:47 -0400

    Update ReplaceElementsWithGreatestElementOnRightSide.py

--- Leetcode-Arrays-101/ReplaceElementsWithGreatestElementOnRightSide.py
+++ Leetcode-Arrays-101/ReplaceElementsWithGreatestElementOnRightSide.py
@@ -49,3 +49,13 @@ class Solution:
 # time complexity - O(n) - because of for loop
 # space complexity - O(1) 
 '''
+
+# below is more concise way to write Solution 2:-
+class Solution(object):
+    def replaceElements(self, arr):
+        me,arr[-1] = arr[-1],-1
+        for i in range(len(arr)-2,-1,-1):
+            arr[i],me = me,max(me,arr[i])   
+        return arr
+
+      

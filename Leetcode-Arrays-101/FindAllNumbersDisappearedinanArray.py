14f5eb2a801b5996e4522e0b512b3a684028150f
Neeraj Somani <neerajsomani04@gmail.com>
2020-10-14 11:50:16 -0400

    Create FindAllNumbersDisappearedinanArray.py

---
+++ Leetcode-Arrays-101/FindAllNumbersDisappearedinanArray.py
@@ -0,0 +1,20 @@
+# Lets follow our procedure to solve this question:
+'''
+Definition - can be understood from question (https://leetcode.com/explore/learn/card/fun-with-arrays/523/conclusion/3231/)
+Data - 
+  - Input - a array that contains some intergers
+  - output - return third maximum number in array
+  - Edge case - number can vary from any negative to position number, array length can be one to infinity.
+  - assumptions - array can't be empty
+  
+Pseudo code - 
+  - create an empty array that contains only 3 maximum element at any iteration of nums array
+  - run through each element of array and check if new element is greater than min(array) 
+    - replace it
+    - return third max of array after sorting it
+'''
+
+# Actual Code 
+
+
+# Solution 1 -

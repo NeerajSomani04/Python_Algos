81c250678bf206ca27722a8d3845d5218f8e9909
Neeraj Somani <neerajsomani04@gmail.com>
2020-10-14 17:23:43 -0400

    Update PlusOne.py

--- ArrayAndString/PlusOne.py
+++ ArrayAndString/PlusOne.py
@@ -25,6 +25,45 @@ class Solution:
         return digits
 
 ''' Big-O efficiency
-# time complexity - O(n)
+# time complexity - O(n*n)
 # space complexity - O(1)
+
+# Above solution is quadratic solution in terms of time complexity, if we increase the size of n to a great amount. Its not a good solution.
+
+from timeit import timeit
+for e in range(15, 23):
+    digits = 2**e * [1]
+    print(timeit(lambda: [int(i) for i in str(int(''.join(map(str,digits)))+1)], number=1))
+
+0.10307004694835681
+0.3856829295774648
+1.4858273051643196
+5.850462647887324
+23.374171342723006
+94.15480187793428
+383.15695864788734
+1525.5406662910798
 '''
+
+# Solution 2 - Need to understand this solution
+class Solution:
+    def plusOne(self, digits):
+        for i in range(len(digits)):
+            if digits[~i] < 9:
+                digits[~i] += 1
+                return digits
+            digits[~i] = 0
+        return [1] + [0] * len(digits)
+
+
+# Solution 3 - Need to understand this solution
+def plusOne(self, digits):
+    length = len(digits) - 1
+    while digits[length] == 9:
+        digits[length] = 0
+        length -= 1
+    if(length < 0):
+        digits = [1] + digits
+    else:
+        digits[length] += 1
+    return digits

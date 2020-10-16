154acd7abcd10ce8d5c1ce13a64b70ec4d2e7ead
Neeraj Somani <neerajsomani04@gmail.com>
2020-10-16 12:26:18 -0400

    Create AddBinary.py

---
+++ ArrayAndString/AddBinary.py
@@ -0,0 +1,20 @@
+# Lets follow our procedure to solve this question:
+'''
+Definition - can be understood from question (https://leetcode.com/explore/learn/card/array-and-string/203/introduction-to-string/1160/)
+Data - 
+  - Input - Given two binary strings, The input strings are both non-empty and contains only characters 1 or 0.
+  - output - return string that contains sum of two input strings
+  - Edge case - 
+  - assumptions - Constraints:
+        Each string consists only of '0' or '1' characters.
+        1 <= a.length, b.length <= 10^4
+        Each string is either "0" or doesn't contain any leading zero.
+  
+Pseudo code - 
+  - 
+'''
+
+# Notes:- This question is completely out of my understanding right now. Will come to this later.
+
+# Actual Code 
+# Solution 1 -

a99a7378962771e0258d8c999db7bd6d987916bc
Neeraj Somani <neerajsomani04@gmail.com>
2020-10-20 18:30:14 -0400

    Create NumberOfIslands_DFS.py

---
+++ ArrayAndString/NumberOfIslands_DFS.py
@@ -0,0 +1,58 @@
+# Lets follow our procedure to solve this question:
+'''
+Definition - can be understood from question (https://leetcode.com/explore/learn/card/queue-stack/231/practical-application-queue/1374/)
+Data - 
+  - Input - an m x n 2d grid map of '1's (land) and '0's (water)
+  - output - return the number of islands.
+  - Edge case - 
+  - assumptions - grid can't be empty
+  Constraints:
+      m == grid.length
+      n == grid[i].length
+      1 <= m, n <= 300
+      grid[i][j] is '0' or '1'.
+  
+Pseudo code - 
+  - 
+  - 
+'''
+
+# Actual Code 
+# Solution 1 -
+class Solution:
+    def is_valid(self, grid, r, c):
+        m, n = len(grid), len(grid[0])
+        if r < 0 or c < 0 or r >= m or c >= n:
+            return False
+        return True
+    
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if not grid or not grid[0]:
+            return 0
+
+        m, n = len(grid), len(grid[0])
+        count = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    count += 1
+        return count
+
+    def dfs(self, grid, r, c):
+        grid[r][c] = '2'
+        print("grid after updating '1' to '2'", grid)
+        directions = [(0,1), (0,-1), (-1,0), (1,0)]
+        for d in directions:
+            nr, nc = r + d[0], c + d[1]    
+            if self.is_valid(grid, nr, nc) and grid[nr][nc] == '1':
+                self.dfs(grid, nr, nc)
+                
+''' Big-O efficiency 
+# time complexity - O(n*m) - because of 2 for loop
+# space complexity - O(1) - because we don't need any stack or queue to store any value
+'''

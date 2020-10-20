d40dd10f97503fea50cd4f047d02b5959f928cf8
Neeraj Somani <neerajsomani04@gmail.com>
2020-10-19 21:13:16 -0400

    Create NumberOfIslands.py

---
+++ ArrayAndString/NumberOfIslands.py
@@ -0,0 +1,22 @@
+# Lets follow our procedure to solve this question:
+'''
+Definition - can be understood from question (https://leetcode.com/explore/learn/card/queue-stack/231/practical-application-queue/1374/)
+
+Data - 
+  - Input - an m x n 2d grid map of '1's (land) and '0's (water)
+  - output - return the number of islands.
+  - Edge case - 
+  - assumptions - grid can't be empty
+  Constraints:
+      m == grid.length
+      n == grid[i].length
+      1 <= m, n <= 300
+      grid[i][j] is '0' or '1'.
+  
+Pseudo code - 
+  - 
+  - 
+'''
+
+# Actual Code 
+# Solution 1 -

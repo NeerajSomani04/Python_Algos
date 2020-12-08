8a7fca59a7eb26e5fc4d057f5e22ed1cd8850964
Neeraj Somani <neerajsomani04@gmail.com>
2020-12-08 14:31:35 -0500

    Create RemoveLinkedListElements.py

---
+++ Leetcode-LinkedList/RemoveLinkedListElements.py
@@ -0,0 +1,8 @@
+question link -- https://leetcode.com/explore/learn/card/linked-list/219/classic-problems/1207/
+
+input --> head and value that needs to be deleted
+output --> head
+edge case --> always have one element in list
+assumption --> 
+
+

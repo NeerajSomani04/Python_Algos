5a62f093462a574e081c9facc57b9739747ef6b8
Neeraj Somani <neerajsomani04@gmail.com>
2021-01-08 13:26:06 -0500

    Update 09-19-RemoveNthNodeFromEndOfList.py

--- Leetcode-Problems/09-19-RemoveNthNodeFromEndOfList.py
+++ Leetcode-Problems/09-19-RemoveNthNodeFromEndOfList.py
@@ -5,7 +5,7 @@
 # Definition, Data, Pseudo code, Actual code, Big-O efficiency
 
 ## Definition -- Can be easily understood from leetcode problem statement
-
+'''
 ## Data (IOEA) -- Input, Output, Edge Cases, Assumptions
   # Input -- Given the head of a linked list, remove the nth node from the end of the list 
   # Output -- return head of new linked list
@@ -17,15 +17,65 @@
             0 <= Node.val <= 100
             1 <= n <= sz
 
-
+'''
 
 ''' 
 Pseudo code  --
+    - Traverse through list, following ".next"
       - count length of Linked list
       - calculate index where we need to delete the node
-      - tranversing again to delete the node
+      - tranversing again to delete the node, by point around the n node (that we want to delete)
+      - handle edge deletion edge case
+      - 
 '''
 
 
 # Actual code --
 ## Solution 1:
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        # 1. find out length of linked list
+        # 2. identify left index of the node that we need to delete (as per question we are given right index of the node)
+        # 3. traverse the linkedlist and point around the node
+        
+        on = head
+        length = 1
+        while on.next:
+            length += 1
+            on = on.next
+        leftIndex = length - n
+        
+        on = head
+        if leftIndex == 0:
+            return on.next
+         
+        while leftIndex > 1:
+            leftIndex -= 1
+            on = on.next
+        on.next = on.next.next
+        return head
+      
+      
+ ## Aaron has calculated the length in a wrong way but conceptually, explaination is good.
+
+
+## Solution 2 : Solving the same problem in one traversal:
+class Solution:
+    def removeNthFromEnd(self, head, n):
+        fast = slow = head
+        for _ in range(n):
+            fast = fast.next
+        if not fast:
+            return head.next
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return head
+      
+ 

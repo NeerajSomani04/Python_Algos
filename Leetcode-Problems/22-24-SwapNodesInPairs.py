9d56918c2b3b297c4ace74c7324d502271098951
Neeraj Somani <neerajsomani04@gmail.com>
2021-01-12 19:58:25 -0500

    Create 22-24-SwapNodesInPairs.py

---
+++ Leetcode-Problems/22-24-SwapNodesInPairs.py
@@ -0,0 +1,54 @@
+## Solution 1 -- Recursive Solution
+
+class Solution(object):
+    def swapPairs(self, head):
+        if not head or not head.next: return head
+        new_start = head.next.next
+        head, head.next = head.next, head
+        head.next.next = self.swapPairs(new_start)
+        return head
+
+## Another way of Writing the same recursive solution:
+class Solution(object):
+    def swapPairs(self, head):
+        if head and head.next:
+            tmp = head.next
+            head.next = self.swapPairs(tmp.next)
+            tmp.next = head ## swaping is happening after the recursive call
+            return tmp
+        return head
+
+## Solution 2:- Iterative call
+class Solution(object):
+    def swapPairs(self, head):
+        if not head or not head.next: return head
+        dummy = ListNode(0)
+        dummy.next = head
+        cur = dummy
+        
+        while cur.next and cur.next.next:
+            first = cur.next
+            sec = cur.next.next
+            cur.next = sec
+            first.next = sec.next
+            sec.next = first
+            cur = cur.next.next
+        return dummy.next 
+        
+
+def swapPairs1(self, head):
+    dummy = p = ListNode(0)
+    dummy.next = head
+    while head and head.next:
+        tmp = head.next
+        head.next = tmp.next
+        tmp.next = head
+        p.next = tmp
+        head = head.next
+        p = tmp.next
+    return dummy.next
+    
+
+''' Big-O efficiency 
+## time complexity --> O(n) 
+## Space complexity --> 

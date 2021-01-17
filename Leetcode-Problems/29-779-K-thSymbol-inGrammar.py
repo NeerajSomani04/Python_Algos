3b13ed57bfa0d76dae45cb5ff6ecb2d70c58da82
Neeraj Somani <neerajsomani04@gmail.com>
2021-01-17 11:30:46 -0500

    Create 29-779-K-thSymbol-inGrammar.py

---
+++ Leetcode-Problems/29-779-K-thSymbol-inGrammar.py
@@ -0,0 +1,40 @@
+## Notes --> I tried to solve it but unable to understand it. Need to do it again.
+
+# In this we are going to solve leetcode #779 problem
+# https://leetcode.com/problems/k-th-symbol-in-grammar/
+
+# we are going to follow a specific process (D, D, P, A, B)
+# Definition, Data, Pseudo code, Actual code, Big-O efficiency
+
+## Definition -- Can be easily understood from leetcode problem statement
+
+## Data (IOEA) -- Input, Output, Edge Cases, Assumptions
+  # Input -- N and K
+  # Output -- Kth value in Nth row
+  # Egde Cases -- first row and first element
+  # Assumptions -- 
+
+''' Pseudo code  --
+
+'''
+
+# Actual code --
+# Solution 1: my naive solution (time limit exceeded in leetcode)
+
+class Solution:
+    def kthGrammar(self, N: int, K: int) -> int:
+        n = 1
+        first = [0]
+        second = []
+        while N > n:
+            for i in first:
+                if i == 0:
+                    second = second + [0,1]
+                elif i == 1:
+                    second = second + [1,0]
+            first = second
+            second = []
+            n = n+1
+        print(first)
+        return first[K-1]
+

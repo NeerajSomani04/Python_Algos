4e9d3eb79b1714cd6b6d23997a6be65760120451
Neeraj Somani <neeraj.somani4@outlook.com>
2021-01-24 17:05:22 -0500

    Update 34-136-SingleNumber.py

--- Leetcode-Problems/34-136-SingleNumber.py
+++ Leetcode-Problems/34-136-SingleNumber.py
@@ -20,4 +20,67 @@
 '''
 
 # Actual code --
-# Solution 1:
+# Solution 1: hash table solution
+from collections import defaultdict
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hash_table = defaultdict(int)
+        for i in nums:
+            hash_table[i] += 1
+        
+        for i in hash_table:
+            if hash_table[i] == 1:
+                return i
+
+''' Big-O efficiency
+Space complexity - O(n) - because of hash table
+time complexity - O(n) - because of for loop
+'''
+
+# Solution 2: sort array and then loop through
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        i = 1
+        result = list()
+        while i < len(nums):
+            if nums[i-1]==nums[i]:
+                nums.pop(i)
+                nums.pop(i-1)
+                i-=1
+            i+=1
+        return nums[0]
+
+''' Big-O efficiency
+Space complexity - O(1) - because of hash table
+time complexity - O(n long n) - because of for loop
+runtime - 196 ms
+memory utilization - 15.7 MB
+'''
+
+# Solution 3: math logic implementation 
+class Solution(object):
+    def singleNumber(self, nums):
+        return 2 * sum(set(nums)) - sum(nums)
+
+''' Big-O efficiency
+Space complexity - O(n) - because of hash table
+time complexity - O(n) - because of for loop
+runtime - 132 ms
+memory utilization - 16.7 MB
+'''
+
+# Solution 4: Bit manipulation
+class Solution(object):
+    def singleNumber(self, nums):
+        a = 0
+        for i in nums:
+            a ^= i
+        return a
+
+''' Big-O efficiency
+Space complexity - O(1) - because of hash table
+time complexity - O(n) - because of for loop
+runtime - 128 ms
+memory utilization - 16.5 MB
+'''

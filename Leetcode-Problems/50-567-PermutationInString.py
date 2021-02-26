8194c85c7624e20cea46ca470992e2be42fe9821
Neeraj Somani <neeraj.somani4@outlook.com>
2021-02-26 10:20:21 -0500

    Create 50-567-PermutationInString.py

---
+++ Leetcode-Problems/50-567-PermutationInString.py
@@ -0,0 +1,13 @@
+# 567 - https://leetcode.com/problems/permutation-in-string/
+
+# what is permutation -- https://www.britannica.com/science/permutation
+
+'''
+Input - given two strings s1 and s2
+output - return true if s2 contains the permutation of s1, else return False
+edge case - 
+assumptions - 
+Constraints:
+    The input strings only contain lower case letters.
+    The length of both given strings is in range [1, 10,000].
+'''

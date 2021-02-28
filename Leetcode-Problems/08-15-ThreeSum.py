a8abcb2b6d45819bff71904338822e8818794c47
Neeraj Somani <neeraj.somani4@outlook.com>
2021-02-28 14:42:10 -0500

    Update 08-15-ThreeSum.py

--- Leetcode-Problems/08-15-ThreeSum.py
+++ Leetcode-Problems/08-15-ThreeSum.py
@@ -98,3 +98,27 @@ class Solution:
     - else, O(n) - if we consider output array as well
 '''
 
+## Solution 3: A different way of writing same question:
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        if n < 3:
+            return []
+        result = set()
+        nums.sort()
+        for i in range(n):
+            left = i+1
+            right = n-1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total == 0:
+                    result.add((nums[i], nums[left], nums[right]))
+                    left += 1
+                elif total > 0:
+                    right -= 1
+                else:
+                    left += 1
+        return result
+      
+      

808b662b63f94e8f881191330b26c8be4ac7e4b2
Neeraj Somani <neeraj.somani4@outlook.com>
2021-03-03 17:28:48 -0500

    Update 61-442-FindAllDuplicatesInArray.py

--- Leetcode-Problems/61-442-FindAllDuplicatesInArray.py
+++ Leetcode-Problems/61-442-FindAllDuplicatesInArray.py
@@ -26,5 +26,21 @@ time complexity - O(n)
 space complexity - O(1)
 '''
 
-# Solution 2: Need to solve same using binary approach
+# Solution 2: a cleaner way to write same
+class Solution(object):
+    def findDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        res = []
+        for x in nums:
+            if nums[abs(x)-1] < 0:
+                res.append(abs(x))
+            else:
+                nums[abs(x)-1] *= -1
+        return res
+
+# Solution 3:
+
 

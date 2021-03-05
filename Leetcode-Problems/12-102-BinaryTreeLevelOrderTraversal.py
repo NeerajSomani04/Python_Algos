86699869f92c4cfb85fe8657a9cc09c6c91950cf
Neeraj Somani <neeraj.somani4@outlook.com>
2021-03-05 18:01:33 -0500

    Update 12-102-BinaryTreeLevelOrderTraversal.py

--- Leetcode-Problems/12-102-BinaryTreeLevelOrderTraversal.py
+++ Leetcode-Problems/12-102-BinaryTreeLevelOrderTraversal.py
@@ -59,7 +59,44 @@ space complexity --> O(1) --> considering output levels array will always be the
 time complexity --> O(n) --> because of upper while loop, inner while loop in only 'k' times which is maximum number of node at any given level.
 '''
 
-
+# Solution 2: Same solution, but different way of writing
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        '''
+        Input - a root node of tree
+        Output - a list of all nodes at each level
+        Edge case - empty input, 
+        assumption - 
+        '''
+        
+        if root is None:
+            return
+        
+        queue = [root]
+        result = []
+        
+        while queue:
+            len_q = len(queue)
+            list_level = []
+            while len_q > 0:
+                curr = queue.pop(0)
+                len_q -= 1
+                list_level.append(curr.val)
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+            result.append(list_level)
+        
+        return result
+        
+        
 
 
 

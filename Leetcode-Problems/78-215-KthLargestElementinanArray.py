4d58c3a0c041af5f7d29c84cc90457a7aa70dc7f
Neeraj Somani <neeraj.somani4@outlook.com>
2021-03-09 10:11:34 -0500

    Update 78-215-KthLargestElementinanArray.py

--- Leetcode-Problems/78-215-KthLargestElementinanArray.py
+++ Leetcode-Problems/78-215-KthLargestElementinanArray.py
@@ -36,3 +36,34 @@ As discussed above, the time complexity of this algorithm is O(K*logK+(N-K)*logK
 Space complexity #
 The space complexity will be O(K)O(K) since we need to store the top ‘K’ numbers in the heap.
 '''
+
+
+'''
+below code is to find kth smallest element in the array
+import heapq
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        '''
+        Edge cases -- numbers could be negative,
+            -- numbers could be repetitive
+            -- need to return kth largest item from array
+        '''
+        
+        heapq.heapify(nums) # creating a heapify is a O(log n) opertaion
+        
+        k_elements = [-x for x in nums[:k]]
+        
+        heapq.heapify(k_elements) # creating a heapify is a O(log n) opertaion [1,2]
+        # remember this is min heap, hence, smallest element will always be at the top
+        
+        for i in range(k, len(nums)): # this is O(n) operation
+            if -nums[i] > k_elements[0]:
+                heapq.heappop(k_elements) # this is O(log k) operation
+                heapq.heappush(k_elements,nums[i]) # this is O(log k) operation
+                
+        return -list(k_elements)[-k]
+        
+        
+        
+        
+       '''

08286c926481d90c3115c5bd4941ebf39d488770
Neeraj Somani <neeraj.somani4@outlook.com>
2021-03-09 11:21:01 -0500

    Update 80-973-K-ClosestPointstoOrigin.py

--- Leetcode-Problems/80-973-K-ClosestPointstoOrigin.py
+++ Leetcode-Problems/80-973-K-ClosestPointstoOrigin.py
@@ -60,6 +60,25 @@ Space complexity
 The space complexity will be O(K) because we need to store ‘K’ point in the heap.
 '''
 
+# Solution 3:
+import heapq
+class Solution:
+    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
+        h = []
+        for p in points:
+            if len(h) <= K-1:
+                heapq.heappush(h,(-p[0]**2-p[1]**2,p))
+            else:
+                if -p[0]**2-p[1]**2 > h[0][0]:
+                    heapq.heapreplace(h,(-p[0]**2-p[1]**2,p))
+        res = []
+        for i in range(K):
+            res.append(heapq.heappop(h)[1])
+        return res
+      
+      
+
+
 # Solution 2:
 import heapq
 class Solution:

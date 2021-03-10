d8655fbd5aef62a2e5798a1ef8fcea240df09071
Neeraj Somani <neeraj.somani4@outlook.com>
2021-03-10 08:52:00 -0500

    Update 81-347-TopKFrequentElements.py

--- Leetcode-Problems/81-347-TopKFrequentElements.py
+++ Leetcode-Problems/81-347-TopKFrequentElements.py
@@ -29,5 +29,27 @@ Space complexity #
 The space complexity will be O(N)O(N). Even though we are storing only ‘K’ numbers in the heap. For the frequency map, however, we need to store all the ‘N’ numbers.
 '''
         
+# Solution 2 :
+from collections import Counter
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]: 
+        # O(1) time 
+        if k == len(nums):
+            return nums
         
+        # 1. build hash map : character and how often it appears
+        # O(N) time
+        count = Counter(nums)   
+        # 2-3. build heap of top k frequent elements and
+        # convert it into an output array
+        # O(N log k) time
+        return heapq.nlargest(k, count.keys(), key=count.get) 
+    
         
+'''
+Time complexity #
+The time complexity of the above algorithm is O(N+N*logK)O(N+N∗logK).
+
+Space complexity #
+The space complexity will be O(N)O(N). Even though we are storing only ‘K’ numbers in the heap. For the frequency map, however, we need to store all the ‘N’ numbers.
+'''
